5c938aa02453362fed13546ddc070fb4375b7ed2
mduraes1994 <mduraes1994@gmail.com>
2018-03-17 00:27:56 -0300

    Add navigation to select level page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the HomePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-home',
@@ -15,11 +8,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    private navCtrl: NavController,
+    private navParams: NavParams
+  ) {}
+
+  public goSelectLevel(): void {
+    this.navCtrl.setRoot('SelectLevelPage');
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad HomePage');
-  }
+  ionViewDidLoad() {}
 
 }
